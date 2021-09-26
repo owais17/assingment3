6b423b58269660bbab3affeba28c76fcdba10211
owais17 <57414701+owais17@users.noreply.github.com>
2021-09-26 15:31:13 +0500

    Add files via upload

---
+++ assignment 3.dart
@@ -0,0 +1,48 @@
+void main() {
+// // qUESTION No 1  
+  
+  
+//  final List<dynamic> inputs =
+//   ["Ball","Ball","Ball","Ball","Ball","Ball","Ball","Ball","Ball","Ball"];
+//   List<dynamic> nonDuplicateList=[];
+//   for(final i in inputs  ){
+    
+//     if(!nonDuplicateList.contains(i)){
+      
+//       nonDuplicateList.add(i);
+      
+//     }
+//   }
+  
+//   print(nonDuplicateList);
+  
+  
+//  List<dynamic> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+// List<dynamic> b=a;
+
+
+    
+//     if (b % 2 == 0) {
+//         print(b);
+//     }
+//     else {
+//         print("Chosen number is odd");
+//     }
+  //question 6
+// List<dynamic> fruits = ['apple', 'banana' ,'mango'];
+
+// print(fruits);
+
+//   
+  
+  //question number 5
+ 
+//   var i=7;
+//   for (i=0; i<=15; i++) {
+  
+//   print(i*2);
+//   }
+//
+
+}
+  // Change here to change output
